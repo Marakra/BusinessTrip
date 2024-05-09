ccefda0b55a5728c6c7ebfb07e094eb38ad033fb
Marianna Popovych <m.popovych777@gmail.com>
2024-05-09 21:19:20 +0200

    JJDZR14KA-14: change format file to json

--- Fitbit/src/main/java/org/infoshere/App.java
+++ Fitbit/src/main/java/org/infoshere/App.java
@@ -14,58 +14,16 @@ import java.util.Scanner;
  */
 public class App {
 
-
     public static void main(String[] args) throws IOException {
         FileService fileService = new FileService();
         Scanner scanner = new Scanner(System.in);
-       // System.out.print("Create name file: ");
-       // String nameFile = scanner.nextLine();
-       // System.out.println("Enter here your data: ");
-       // String content = scanner.nextLine();
+        System.out.print("Create name file: ");
+        String nameFile = scanner.nextLine();
+        System.out.println("Enter here your data: ");
+        String content = scanner.nextLine();
 
-//        fileService.createFile(
-//                nameFile
-//        );
-
-      //  fileService.writeToFile(nameFile, content);
+        fileService.writeToFile(nameFile, content);
 
         fileService.readFromFile("qwe");
     }
-
-
-
-
-
-
-
-
-
-    private static void menu() {
-        MenuService menuService = new MenuService();
-
-        int optionNumber;
-
-        do {
-            menuService.displayMenu();
-            optionNumber = menuService.getChoice();
-            switch (optionNumber) {
-                case 1:
-                    menuService.trainersList();
-
-                    break;
-                case 2:
-                    menuService.activityList();
-                    break;
-            }
-            if (optionNumber > 3){
-                System.out.println("------------------------------------");
-                System.out.println("Off the scale. Please enter a valid number.");
-                System.out.println("------------------------------------");
-            }
-        }
-        while (optionNumber != 3);
-        menuService.exit();
-    }
-
-
 }
