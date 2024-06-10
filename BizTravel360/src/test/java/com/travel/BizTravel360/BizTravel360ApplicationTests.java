feb33d3cac47a77db4fbfb5744671f8088f545d6
Kamil S <kamil.slojkowski@gmail.com>
2024-06-10 10:41:12 +0200

    BizTravel360

---
+++ BizTravel360/src/test/java/com/travel/BizTravel360/BizTravel360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.BizTravel360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizTravel360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
