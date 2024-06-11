3e391bad4fc812532b9d838159a208b6c71d2b24
Marianna Popovych <m.popovych777@gmail.com>
2024-06-11 21:58:19 +0200

    JJDZR14KA-25: changes - add FileRepository - implements FileRepository in FileService

--- BizTravel360/src/main/java/com/travel/BizTravel360/file/FileService.java
+++ BizTravel360/src/main/java/com/travel/BizTravel360/file/FileService.java
@@ -1,4 +1,4 @@
-package com.travel.BizTravel360.json;
+package com.travel.BizTravel360.file;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -8,18 +8,21 @@ import java.io.File;
 import java.io.IOException;
 
 @Component
-public class Json {
+public class FileService implements FileRepository {
 
     private final ObjectMapper objectMapper;
     
-    public Json(ObjectMapper objectMapper) {
+    public FileService(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
     }
     
+    
+    @Override
     public void writerToFile(Object object, String fileName) throws IOException {
         objectMapper.writeValue(new File(fileName), object);
     }
     
+    @Override
     public <T> T readFromFile(Class<T> valueType, String fileName) throws IOException {
         return objectMapper.readValue(new File(fileName), valueType);
     }
