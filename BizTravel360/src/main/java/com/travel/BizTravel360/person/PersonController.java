dce7aea01ec28d5482cdc1eff1ceadc0fedd6d01
Marianna Popovych <m.popovych777@gmail.com>
2024-06-30 00:40:08 +0200

    JJDZR14KA-35 + JJDZR14KA-30 - create new person - read all people

--- BizTravel360/src/main/java/com/travel/BizTravel360/person/PersonController.java
+++ BizTravel360/src/main/java/com/travel/BizTravel360/person/PersonController.java
@@ -1,8 +1,50 @@
 package com.travel.BizTravel360.person;
 
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.IOException;
+import java.util.List;
 
 @Controller
-public class PersonController {
 
+public class PersonController {
+    
+    private final PersonService personService;
+    
+   @Autowired
+   public PersonController(PersonService personService) {
+       this.personService = personService;
+   }
+   
+    @GetMapping("/people")
+    public String getAllPeople(Model model) {
+        try {
+            List<Person> people = personService.readAll();
+            model.addAttribute("people", people);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load people data", e);
+        }
+        return "menu_categories/people";
+    }
+    
+    @GetMapping("createPersonForm")
+    public String createPersonForm(Model model) {
+       model.addAttribute("person", new Person());
+       return "createObjects/createPersonForm";
+    }
+    
+    @PostMapping("createPerson")
+    public String createPerson(@ModelAttribute Person person) {
+       try {
+           personService.createPerson(person);
+           return "redirect:people";
+       } catch (IOException e) {
+           throw new RuntimeException("Failed to create new person", e);
+       }
+    }
 }
