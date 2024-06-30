6e4f863fae72ef8ccdfee319b72bc4c49c19b7b8
Marianna Popovych <m.popovych777@gmail.com>
2024-06-30 09:18:23 +0200

    JJDZR14KA-35 + JJDZR14KA-30 - create new person - read all people

--- BizTravel360/src/main/java/com/travel/BizTravel360/person/PersonController.java
+++ BizTravel360/src/main/java/com/travel/BizTravel360/person/PersonController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
-
 public class PersonController {
     
     private final PersonService personService;
@@ -25,11 +25,17 @@ public class PersonController {
     public String getAllPeople(Model model) {
         try {
             List<Person> people = personService.readAll();
+            
+            Collections.reverse(people);
+            
+            for (int i = 0; i < people.size(); i++) {
+                people.get(i).setPersonId(i+1);
+            }
             model.addAttribute("people", people);
         } catch (IOException e) {
             throw new RuntimeException("Failed to load people data", e);
         }
-        return "menu_categories/people";
+        return "pages/people";
     }
     
     @GetMapping("createPersonForm")
